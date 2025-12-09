bce60204637446f500b504f2c6e725fd0ce81a8e
Ei Khaing Sin <eikhaingsin@cp.com.mm>
2025-12-09 16:26:24 +0630

    update

---
+++ src/component/switpro/fullname.jsx
@@ -0,0 +1,8 @@
+const FullNameInput = () => (
+    <input
+        type="text"
+        placeholder="Full Name"
+        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+    />
+);
+export default FullNameInput;
